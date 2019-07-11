Add evaluation scripts Add evaluation to train process every 500 steps Improve training logging Fix decode formula bug

data_preprocess.py
@@ -22,12 +22,10 @@ class DataHandler:
 
     def __fit_tokenizer(self):
         if os.path.isfile(config.vocab_path):
-            print('Load tokenizer')
             with open(config.vocab_path, 'r') as f:
                 json_content = f.read()
                 self.tokenizer = tokenizer_from_json(json_content)
         else:
-            print('Start fitting tokenizer')
             tmp_doc = (self.beg_token + ' ' + self.end_token + ' ') * 100
             docs = [tmp_doc, self.__read_raw_formulas('train')]
             num_tokens = config.vocab_size - 3  # for beg, and, unk token
@@ -36,15 +34,14 @@ class DataHandler:
             self.tokenizer.fit_on_texts(docs)
             with open(config.vocab_path, 'w+') as f:
                 f.write(self.tokenizer.to_json())
-            print('Fitting tokenizer finished')
 
-    def __get_path(self, mode):
+    def get_path(self, mode):
         formulas_path = os.path.join(self.formula_path, '{}_formulas.txt'.format(mode))
         images_folder = os.path.join(self.images_path, 'images_{}'.format(mode))
         return formulas_path, images_folder
 
     def __read_raw_formulas(self, mode, split=False):
-        path = self.__get_path(mode)[0]
+        path = self.get_path(mode)[0]
         with open(path, 'r') as f:
             content = f.read()
             if split:
@@ -68,7 +65,7 @@ class DataHandler:
         return result
 
     def read_images(self, mode, index):
-        dir_path = self.__get_path(mode)[1]
+        dir_path = self.get_path(mode)[1]
         images_data = []
         for i in index:
             file_path = os.path.join(dir_path, str(i) + '.png')
@@ -78,19 +75,21 @@ class DataHandler:
         data = np.array(images_data)
         return data
 
-    def decode_formula(self, sequence):
-        formula = self.tokenizer.sequences_to_texts([sequence])[0]
-        start_idx, end_idx = 0, len(formula)
-        try:
-            start_idx = formula.index(self.beg_token)
-            start_idx += len(self.beg_token)
-        except:
-            pass
-        try:
-            end_idx = formula.index(self.end_token)
-        except:
-            pass
-        return formula[start_idx+1:end_idx]
+    def decode_formula(self, sequences):
+        def normalize(formula):
+            start_idx, end_idx = 0, len(formula)
+            if formula[:6] == '<BOS> ':
+                start_idx = 6
+            try:
+                end_idx = formula.index(self.end_token)
+            except:
+                pass
+            return formula[start_idx:end_idx]
+
+        sequences_list = sequences.tolist()
+        formulas = self.tokenizer.sequences_to_texts(sequences_list)
+        formulas = [normalize(formula) for formula in formulas]
+        return formulas
 
     def plot_sample_sizes(self):
         lines = self.__read_raw_formulas('train', split=True)

eval/bleu_score.py
@@ -2,7 +2,6 @@ import sys, argparse
 import nltk
 
 
-
 def process_args(args):
     parser = argparse.ArgumentParser(description='Evaluate text edit distance.')
 
@@ -28,6 +27,29 @@ def process_args(args):
     return parameters
 
 
+def bleu_eval(target_formulas, predicted_formulas, ngram=5):
+    total_bleu_score = 0
+    n = len(target_formulas)
+    if len(target_formulas) != len(predicted_formulas):
+        print("number of formulas doesn't match")
+        return
+
+    for tf, pf in zip(target_formulas, predicted_formulas):
+        tf_ = tf.strip()
+        pf_ = pf.strip()
+        true_token = tf_.split(' ')
+        predicted_tokens = pf_.split(' ')
+        l = min(ngram, len(true_token), len(predicted_tokens))
+        if l == 0:
+            print("Encountered formula with zero length")
+        bleu_score = nltk.translate.bleu_score.sentence_bleu([true_token], predicted_tokens,
+                                                             weights=[1.0 / l for _ in range(l)])
+        total_bleu_score += bleu_score
+    score = float(total_bleu_score) / n
+    print('BLEU Score: %f' % score)
+    return score
+
+
 def main(args):
     parameters = process_args(args)
 
@@ -38,30 +60,7 @@ def main(args):
     target_formulas = open(target_formulas_file).readlines()
     predicted_formulas = open(predicted_formulas_file).readlines()
 
-    i = 0
-
-    total_bleu_score = 0
-    if len(target_formulas) != len(predicted_formulas):
-        print("number of formulas doesn't match")
-        return
-    n = len(target_formulas)
-    for tf, pf in zip(target_formulas, predicted_formulas):
-        i += 1
-        if i % 2000 == 0:
-            print("{}/{}".format(i, n))
-
-        tf_ = tf.strip()
-        pf_ = pf.strip()
-        true_token = tf_.split(' ')
-        predicted_tokens = pf_.split(' ')
-        l = min(ngram, len(true_token), len(predicted_tokens))
-        if l == 0:
-            print("Encountered formula with zero length")
-        bleu_score = nltk.translate.bleu_score.sentence_bleu([true_token], predicted_tokens,
-                                                                weights=[1.0 / l for _ in range(l)])
-        total_bleu_score += bleu_score
-    print("{}/{}".format(n, n))
-    print('BLEU Score: %f' % (float(total_bleu_score) / n))
+    bleu_eval(target_formulas, predicted_formulas, ngram)
 
 
 if __name__ == '__main__':

eval/edit_distance.py
@@ -21,6 +21,27 @@ def process_args(args):
     return parameters
 
 
+def edit_distance_eval(target_formulas, predicted_formulas):
+    total_len = 0
+    total_edit_distance = 0
+    if len(target_formulas) != len(predicted_formulas):
+        print("number of formulas doesn't match")
+        return
+
+    for tf, pf in zip(target_formulas, predicted_formulas):
+        tf_ = tf.strip()
+        pf_ = pf.strip()
+        true_token = tf_.split(' ')
+        predicted_tokens = pf_.split(' ')
+        l = max(len(true_token), len(predicted_tokens))
+        edit_distance = distance.levenshtein(true_token, predicted_tokens)
+        total_len += l
+        total_edit_distance += edit_distance
+    score = 1. - float(total_edit_distance) / total_len
+    print('Edit Distance Accuracy: %f' % score)
+    return score
+
+
 def main(args):
     parameters = process_args(args)
 
@@ -30,29 +51,7 @@ def main(args):
     target_formulas = open(target_formulas_file).readlines()
     predicted_formulas = open(predicted_formulas_file).readlines()
 
-    i = 0
-
-    total_len = 0
-    total_edit_distance = 0
-    if len(target_formulas) != len(predicted_formulas):
-        print("number of formulas doesn't match")
-        return
-    n = len(target_formulas)
-    for tf, pf in zip(target_formulas, predicted_formulas):
-        i += 1
-        if i % 2000 == 0:
-            print("{}/{}".format(i, n))
-
-        tf_ = tf.strip()
-        pf_ = pf.strip()
-        true_token = tf_.split(' ')
-        predicted_tokens = pf_.split(' ')
-        l = max(len(true_token), len(predicted_tokens))
-        edit_distance = distance.levenshtein(true_token, predicted_tokens)
-        total_len += l
-        total_edit_distance += edit_distance
-    print("{}/{}".format(n, n))
-    print('Edit Distance Accuracy: %f' % (1. - float(total_edit_distance) / total_len))
+    edit_distance_eval(target_formulas, predicted_formulas)
 
 
 if __name__ == '__main__':

train.py
@@ -1,9 +1,14 @@
 import os
+import logging
 import tensorflow as tf
+import numpy as np
 
 from config import config
 from data_generator import DataGenerator
 from model.image_to_latex import ImageToLatexModel
+from eval.evaluation import evaluation
+
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -13,24 +18,45 @@ flags.DEFINE_string('model_name', 'image2latex', 'Model name')
 
 
 def main(args):
-    data_gen = DataGenerator('train')
-    start_token, pad_token = data_gen.data.start_token(), data_gen.data.pad_token()
+    def log(msg, new_section=False):
+        logging.info(msg)
+        if new_section:
+            logging.info('------------------------------------')
+
+    train_set = DataGenerator('train')
+    start_token, pad_token = train_set.data.start_token(), train_set.data.pad_token()
 
     model_dir = os.path.join('runs', FLAGS.model_name)
 
-    print('Start building graph')
+    log('Start building graph')
     tf.reset_default_graph()
     model = ImageToLatexModel(start_token, pad_token)
-    print('Graph building finished!')
+    log('Graph building finished!', True)
 
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
 
-    print('Start fitting ...')
-    for epoch, percentage, images, formulas, _ in data_gen.generator(config.n_epochs):
+    log_every = config.log_every
+    eval_every = config.eval_every
+    log_template = 'Epoch {}({}), step = {} => Loss avg: {}'
+
+    log('Start fitting ...')
+
+    loss_history = []
+    for epoch, percentage, images, formulas, _ in train_set.generator(config.n_epochs):
         loss, step = model.train_step(sess, images, formulas)
-        print('loss:', loss)
-        print('step:', step)
+        loss_history += [loss]
+        # step += 1
+
+        if step % log_every == 0:
+            epoch += 1
+            percentage = '{0:2.2f}%'.format(100 * percentage)
+            loss_average = np.mean(np.array(loss_history))
+            log(log_template.format(epoch, percentage, step, loss_average))
+            loss_history = []
+
+        if step % eval_every == 0:
+            exact_match, bleu, edit_distance = evaluation(session=sess, model=model)
 
 
 if __name__ == '__main__':