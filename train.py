baf660fdc9025a20979c6e2effda52cc914cb311
Ali Karimi <ali1374karimi@gmail.com>
2019-07-11 20:51:33 +0430

    Add model saver increase learning rate

--- train.py
+++ train.py
@@ -15,6 +15,7 @@ tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 flags = tf.app.flags
 flags.DEFINE_string('f', '', 'kernel')
 flags.DEFINE_string('model_name', 'image2latex', 'Model name')
+flags.DEFINE_boolean('load_from_previous', False, 'Whether to load from previous session or not')
 flags.DEFINE_boolean('check_on_small_data', False, 'Train model on 2% of data to figure out model can overfit or not')
 FLAGS = flags.FLAGS
 
@@ -28,7 +29,8 @@ def main(args):
     train_set = DataGenerator('train')
     start_token, pad_token = train_set.data.start_token(), train_set.data.pad_token()
 
-    model_dir = os.path.join('runs', FLAGS.model_name)
+    model_path = os.path.join('runs', FLAGS.model_name + '.ckpt')
+    saver = tf.train.Saver()
 
     gpu_is_available = tf.test.is_gpu_available()
     log('GPU is available: ' + str(gpu_is_available))
@@ -39,15 +41,15 @@ def main(args):
 
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
+    if FLAGS.load_from_previous:
+        saver.restore(sess, model_path)
 
     loss_history = []
     small_data = FLAGS.check_on_small_data
-    n_epochs, per_limit = (300, 0.02) if small_data else (config.n_epochs, None)
+    n_epochs, per_limit = (500, 0.02) if small_data else (config.n_epochs, None)
     validation_set = 'train' if small_data else 'validation'
 
     log_every = config.log_every if gpu_is_available else 1
-    if small_data:
-        log_every *= 4
     eval_every_epoch = config.eval_every_epoch if not small_data else n_epochs // 5
     log_template = 'Epoch {}({}), step = {} => Loss avg: {}'
 
@@ -67,8 +69,12 @@ def main(args):
         if epoch % eval_every_epoch == 0 and percentage_condition:
             exact_match, bleu, edit_distance = evaluation(session=sess, model=model,
                                                           mode=validation_set, percent_limit=per_limit)
+            if not small_data:
+                saver.save(sess, model_path, step)
 
     evaluation(session=sess, model=model, mode=validation_set, percent_limit=per_limit)
+    if not small_data:
+        saver.save(sess, model_path, step)
 
 
 if __name__ == '__main__':
