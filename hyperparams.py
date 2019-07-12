9cda54bfead199f9058b0da362b1ed9c3422e990
Ali Karimi <ali1374karimi@gmail.com>
2019-07-12 22:04:18 +0430

    no learning

--- hyperparams.py
+++ hyperparams.py
@@ -16,7 +16,7 @@ HyperParams = namedtuple('HyperParams',
 
 h_params = HyperParams(
     batch_size=32,
-    learning_rate=1e-10,
+    learning_rate=0,
     cnn_first_layer_filters=64,
     add_positional_embed=False,
     row_encoder_rnn_dim=None,
