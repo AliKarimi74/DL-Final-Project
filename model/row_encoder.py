1e8b754c96ff922267821cc5434037320ea4df5b
Ali Karimi <ali1374karimi@gmail.com>
2019-07-12 19:19:18 +0430

    Add ability to remove row encoder

--- model/row_encoder.py
+++ model/row_encoder.py
@@ -11,15 +11,17 @@ class RowEncoder(tf.keras.Model):
         with tf.variable_scope('row_encoder', reuse=tf.AUTO_REUSE):
             gpu_available = tf.test.is_gpu_available()
             cell = tf.keras.layers.CuDNNLSTM if gpu_available else tf.keras.layers.LSTM
-            self.fw_rnn = cell(units=h_params.row_encoder_rnn_dim,
-                               return_sequences=True,
-                               return_state=False)
-            self.bw_rnn = cell(units=h_params.row_encoder_rnn_dim,
-                               return_sequences=True,
-                               return_state=False,
-                               go_backwards=True)
-            self.rnn_layer = tf.keras.layers.Bidirectional(layer=self.fw_rnn, backward_layer=self.bw_rnn,
-                                                           name='bi_lstm')
+            self.units = h_params.row_encoder_rnn_dim
+            if self.units is not None:
+                self.fw_rnn = cell(units=self.units,
+                                   return_sequences=True,
+                                   return_state=False)
+                self.bw_rnn = cell(units=self.units,
+                                   return_sequences=True,
+                                   return_state=False,
+                                   go_backwards=True)
+                self.rnn_layer = tf.keras.layers.Bidirectional(layer=self.fw_rnn, backward_layer=self.bw_rnn,
+                                                               name='bi_lstm')
 
     def call(self, encoded_images, **kwargs):
         with tf.variable_scope('row_encoder', reuse=tf.AUTO_REUSE):
@@ -27,8 +29,12 @@ class RowEncoder(tf.keras.Model):
             n_rows, n_cols, n_channels = shape[1:4]
             image_rows = tf.reshape(encoded_images, shape=[-1, n_cols, n_channels])
 
-            out = self.rnn_layer(image_rows)
-            out = tf.reshape(out, shape=[-1, n_rows * n_cols, 2 * h_params.row_encoder_rnn_dim])
+            final_channels = n_channels
+            out = image_rows
+            if self.units is not None:
+                final_channels = 2 * h_params.row_encoder_rnn_dim
+                out = self.rnn_layer(out)
+            out = tf.reshape(out, shape=[-1, n_rows * n_cols, final_channels])
 
             return out
 
